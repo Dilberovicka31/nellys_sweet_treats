66cd7d87475d8b280818985560c915fcbd767ce9
Mija Dilberovic <dilberovicm36@gmail.com>
2024-06-30 13:58:52 -0700

    added video and more images

--- src/components/About/index.jsx
+++ src/components/About/index.jsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import React from "react";
 
 function About() {
   return (
-    <div>About</div>
-  )
+    <div>
+      <video
+        controls
+        style={{ width: "50%", maxWidth: "800px", height: " 400px",marginBottom: "24px" }}
+      >
+        <source src="/assets/images/Flan.mp4" type="video/mp4" />
+        Your browser does not support the video tag.
+      </video>
+    </div>
+  );
 }
 
-export default About
+export default About;
--- src/components/Cheesecakes/index.jsx
+++ src/components/Cheesecakes/index.jsx
@@ -1,42 +1,79 @@
-import React from 'react';
-import { Container, Typography, useMediaQuery, useTheme } from '@mui/material';
-import ImageList from '@mui/material/ImageList';
-import ImageListItem from '@mui/material/ImageListItem';
-import ImageListItemBar from '@mui/material/ImageListItemBar';
-import IconButton from '@mui/material/IconButton';
-import StarBorderIcon from '@mui/icons-material/StarBorder';
+import React from "react";
+import {
+  Container,
+  Typography,
+  useMediaQuery,
+  useTheme,
+  Box,
+} from "@mui/material";
+import ImageList from "@mui/material/ImageList";
+import ImageListItem from "@mui/material/ImageListItem";
+import ImageListItemBar from "@mui/material/ImageListItemBar";
+import IconButton from "@mui/material/IconButton";
+import StarBorderIcon from "@mui/icons-material/StarBorder";
 
 function Cheesecakes() {
   const theme = useTheme();
-  const isSmallScreen = useMediaQuery(theme.breakpoints.down('sm'));
+  const isSmallScreen = useMediaQuery(theme.breakpoints.down("sm"));
 
   function srcset(image, width, height, rows = 1, cols = 1) {
     return {
       src: `${image}?w=${width * cols}&h=${height * rows}&fit=crop&auto=format`,
-      srcSet: `${image}?w=${width * cols}&h=${height * rows}&fit=crop&auto=format&dpr=2 2x`,
+      srcSet: `${image}?w=${width * cols}&h=${
+        height * rows
+      }&fit=crop&auto=format&dpr=2 2x`,
     };
   }
 
   return (
     <Container
       sx={{
-        width: isSmallScreen ? '90%' : '70%', // Adjust width based on screen size
-        display: 'flex',
-        flexDirection: 'column',
-        alignItems: 'center',
-        justifyContent: 'center',
-        boxShadow: '0px 8px 16px rgba(0, 0, 0, 0.1)', // Adding shadow
-        padding: '24px',
-        marginTop: '50px',
-        marginBottom: '50px',
+        width: isSmallScreen ? "90%" : "70%", // Adjust width based on screen size
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        justifyContent: "center",
+        boxShadow: "0px 8px 16px rgba(0, 0, 0, 0.1)", // Adding shadow
+        padding: "24px",
+        marginTop: "50px",
+        marginBottom: "50px",
       }}
       maxWidth="lg" //Max Width
     >
       <Typography variant="h4" gutterBottom>
         Welcome to Our Mini Cheesecakes Collection
       </Typography>
+
+      {/* Video section */}
+      <Box
+        width="100%"
+        textAlign="center"
+        mb={4}
+        sx={{
+          maxWidth: "100%",
+          height: "500px",
+          overflow: "hidden",
+          position: "relative",
+          borderRadius: "8px",
+        }}
+      >
+        <video
+          src="/assets/images/minicheesecakes.mp4"
+          controls
+          style={{
+            width: "100%",
+            height: "100%",
+            objectFit: "cover",
+            position: "absolute",
+            top: 0,
+            left: 0,
+          }}
+        />
+      </Box>
+
+      {/* Image list */}
       <ImageList
-        sx={{ width: '100%', height: '100%' }}
+        sx={{ width: "100%", height: "100%" }}
         cols={isSmallScreen ? 1 : 2} // Adjust columns based on screen size
         rowHeight={360}
         gap={24}
@@ -47,18 +84,21 @@ function Cheesecakes() {
               {...srcset(item.img, 300, 300)}
               alt={item.title}
               loading="lazy"
-              style={{ objectFit: 'cover', width: '100%', height: '100%' }}
+              style={{ objectFit: "cover", width: "100%", height: "100%" }}
             />
             <ImageListItemBar
               sx={{
                 background:
-                  'linear-gradient(to bottom, rgba(0,0,0,0.7) 0%, ' +
-                  'rgba(0,0,0,0.3) 70%, rgba(0,0,0,0) 100%)',
+                  "linear-gradient(to bottom, rgba(0,0,0,0.7) 0%, " +
+                  "rgba(0,0,0,0.3) 70%, rgba(0,0,0,0) 100%)",
               }}
               title={item.title}
               position="top"
               actionIcon={
-                <IconButton sx={{ color: 'white' }} aria-label={`star ${item.title}`}>
+                <IconButton
+                  sx={{ color: "white" }}
+                  aria-label={`star ${item.title}`}
+                >
                   <StarBorderIcon />
                 </IconButton>
               }
@@ -73,24 +113,24 @@ function Cheesecakes() {
 
 const itemData = [
   {
-    img: '/assets/images/minicheesecakes.jpeg',
-    title: 'Mini Cheesecakes',
+    img: "/assets/images/minicheesecakes.jpeg",
+    title: "Mini Cheesecakes",
   },
   {
-    img: '/assets/images/minicheesecakes1.jpeg',
-    title: 'Mini Cheesecakes',
+    img: "/assets/images/minicheesecakes1.jpeg",
+    title: "Mini Cheesecakes",
   },
   {
-    img: '/assets/images/minicheesecakes2.jpeg',
-    title: 'Mini Cheesecakes',
+    img: "/assets/images/minicheesecakes2.jpeg",
+    title: "Mini Cheesecakes",
   },
   {
-    img: '/assets/images/minicheesecakes3.jpeg',
-    title: 'Mini Cheesecakes',
+    img: "/assets/images/minicheesecakes3.jpeg",
+    title: "Mini Cheesecakes",
   },
   {
-    img: '/assets/images/minicheesecakes4.jpeg',
-    title: 'Mini Cheesecakes',
+    img: "/assets/images/minicheesecakes4.jpeg",
+    title: "Mini Cheesecakes",
   },
 ];
 
